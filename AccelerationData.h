540005145ac00048fe12106ead4493f5289b63df
Dominic Clifton <me@dominicclifton.name>
2013-02-04 19:12:05 +0000

    Turn AccellerationData into a class and add convenience methods.

--- AccelerationData.h
+++ AccelerationData.h
@@ -3,8 +3,41 @@
 
 #include <stdint.h>
 
-typedef struct AccelerationData_s {
+enum Axis { AXIS_X, AXIS_Y, AXIS_Z };
+
+class AccelerationData {
+public:
   int16_t x, y, z;
-} AccelerationData;
+
+  void setAxisValue(Axis axis, int16_t value) {
+    switch(axis) {
+      case AXIS_X:
+        x = value;
+      break;
+      case AXIS_Y:
+        y = value;
+      break;
+      case AXIS_Z:
+        z = value;
+      break;
+    }
+  }
+
+  int16_t getAxisValue(Axis axis) {
+    switch(axis) {
+      case AXIS_X:
+        return x;
+      break;
+      case AXIS_Y:
+        return y;
+      break;
+      case AXIS_Z:
+        return z;
+      break;
+    }
+    return 0;
+  }
+
+};
 
 #endif
